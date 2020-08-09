5dd74a6dd6ab61bcf617b29bee19d2cd72d95043
David Montesdeoca <backpackerhh@gmail.com>
2020-08-09 19:33:08 +0200

    Resolve max char exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const mappedChars = str.split("").reduce((obj, char) => {
+    return {
+      ...obj,
+      [char]: (obj[char] || 0) + 1,
+    };
+  }, {});
+
+  return Object.keys(mappedChars).reduce((a, b) => (mappedChars[a] > mappedChars[b] ? a : b));
+}
 
 module.exports = maxChar;
