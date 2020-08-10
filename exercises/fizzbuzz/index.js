6d8e357a03d310c9df3b80dc8bdf800f45831f04
David Montesdeoca <backpackerhh@gmail.com>
2020-08-10 20:20:02 +0200

    Resolve fizzbuzz exercise

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,15 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    let value = "";
+
+    if (i % 3 === 0) value += "fizz";
+    if (i % 5 === 0) value += "buzz";
+
+    console.log(value || i);
+  }
+}
 
 module.exports = fizzBuzz;
