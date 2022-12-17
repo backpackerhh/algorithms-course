763fe537d26e3719f6ddf850fa3fad67d4d59d8d
David Montesdeoca <backpackerhh@gmail.com>
2022-12-17 15:08:56 +0100

    Resolve matrix exercise

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,41 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let results = Array(n).fill().map(() => Array(n).fill())
+  let counter = 1
+  let startColumn = 0
+  let endColumn = n - 1
+  let startRow = 0
+  let endRow = n - 1
+
+  while(startColumn <= endColumn && startRow <= endRow) {
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter++
+    }
+
+    startRow++
+
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter++
+    }
+
+    endColumn--
+
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter++
+    }
+
+    endRow--
+
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter++
+    }
+
+    startColumn++
+  }
+
+  return results
+}
 
 module.exports = matrix;
