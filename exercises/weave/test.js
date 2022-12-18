96fe67cf376d3b3cacd9023029cdffe10a12fb14
David Montesdeoca <backpackerhh@gmail.com>
2022-12-18 13:12:59 +0100

    Resolve queue weaving exercise

--- exercises/weave/test.js
+++ exercises/weave/test.js
@@ -43,3 +43,29 @@ test('weave can combine two queues', () => {
   expect(result.remove()).toEqual('four');
   expect(result.remove()).toBeUndefined();
 });
+
+test('weave can combine two queues with different lengths', () => {
+  const one = new Queue();
+  one.add(1);
+  one.add(2);
+  one.add(3);
+  one.add(4);
+  const two = new Queue();
+  two.add('one');
+  two.add('two');
+  two.add('three');
+  two.add('four');
+  two.add('five');
+
+  const result = weave(one, two);
+  expect(result.remove()).toEqual(1);
+  expect(result.remove()).toEqual('one');
+  expect(result.remove()).toEqual(2);
+  expect(result.remove()).toEqual('two');
+  expect(result.remove()).toEqual(3);
+  expect(result.remove()).toEqual('three');
+  expect(result.remove()).toEqual(4);
+  expect(result.remove()).toEqual('four');
+  expect(result.remove()).toEqual('five');
+  expect(result.remove()).toBeUndefined();
+});
